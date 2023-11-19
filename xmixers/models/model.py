a8a4dc481af7146ec90915e82f5f4982096e34a7
Doraemonzzz <715073608@qq.com>
2023-11-19 15:41:20 +0800

    first commit

---
+++ xmixers/models/model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class SequenceModel(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        n_layer = kwargs.get("n_layer", default=1)
+        
+    
+    def get_block_config(self, **kwargs):
+        pass
+    
+    def build_block(self, **kwargs):
+        pass
