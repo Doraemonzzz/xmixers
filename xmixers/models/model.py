cb0fbb2a10e4f588305228b0de8eb3e25a12eb04
Doraemonzzz <715073608@qq.com>
2024-03-20 03:57:04 +0000

    add pre commit hook

--- xmixers/models/model.py
+++ xmixers/models/model.py
@@ -1,14 +1,13 @@
-import torch
 import torch.nn as nn
+
 
 class SequenceModel(nn.Module):
     def __init__(self, **kwargs):
         super().__init__()
         n_layer = kwargs.get("n_layer", default=1)
-        
-    
+
     def get_block_config(self, **kwargs):
         pass
-    
+
     def build_block(self, **kwargs):
         pass
