[Bug Fix] use causal mask when evaluation(not generation)

xmixers/models/transformer/llama/modeling_llama.py
@@ -77,22 +77,24 @@ class LLaMAPreTrainedModel(PreTrainedModel):
     _no_split_modules = ["LLaMALayer"]
 
     def _init_weights(self, module):
-        if isinstance(module, nn.Embedding):
+        if isinstance(module, nn.Linear):
             embedding_dim = module.weight.shape[-1]
             std = embedding_dim**-0.5
-            print("aaa", std)
-            std = 0.02
-            module.weight.data.normal_(mean=0.0, std=std)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.Linear):
-            embedding_dim = module.weight.shape[-1]
-            std = embedding_dim**-0.5
+            print(print(module))
             print("bbb", std)
-            std = 0.02
+            # std = 0.02
             module.weight.data.normal_(mean=0.0, std=std)
             if module.bias is not None:
                 module.bias.data.zero_()
+        elif isinstance(module, nn.Embedding):
+            embedding_dim = module.weight.shape[-1]
+            std = embedding_dim**-0.5
+            print(module)
+            print("aaa", std)
+            # std = 0.02
+            module.weight.data.normal_(mean=0.0, std=std)
+            if module.padding_idx is not None:
+                module.weight.data[module.padding_idx].zero_()
 
     def _set_gradient_checkpointing(self, module, value=False):
         if isinstance(module, LLaMAModel):

xmixers/modules/token_mixers/vanilla_attention/attention.py
@@ -91,9 +91,9 @@ class Attention(nn.Module):
             k, v = past_key_values.update(k, v, self.layer_idx)
 
         if attention_mask is None:
-            output = F.scaled_dot_product_attention(
-                q, k, v, is_causal=True if self.training else False
-            )
+            # use causal when training or evaluation(not for generation)
+            is_causal = True if self.training or q.shape[-2] == k.shape[-2] else False
+            output = F.scaled_dot_product_attention(q, k, v, is_causal=is_causal)
         else:
             assert False, "flash_attn_varlen_qkvpacked_func current not support"
 