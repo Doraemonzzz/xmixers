992b0f8b274a8f3eaa4d38c927881282190576c4
Doraemonzzz <715073608@qq.com>
2024-10-30 10:19:39 +0000

    [Bug Fix] add register for automodel

--- xmixers/__init__.py
+++ xmixers/__init__.py
@@ -1,1 +1,11 @@
+from .models import (
+    GPTConfig,
+    GPTForCausalLM,
+    GPTLayer,
+    GPTModel,
+    LLaMAConfig,
+    LLaMAForCausalLM,
+    LLaMALayer,
+    LLaMAModel,
+)
 from .ops import *
--- xmixers/models/transformer/llama/modeling_llama.py
+++ xmixers/models/transformer/llama/modeling_llama.py
@@ -78,20 +78,12 @@ class LLaMAPreTrainedModel(PreTrainedModel):
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
-            embedding_dim = module.weight.shape[-1]
-            std = embedding_dim**-0.5
-            print(print(module))
-            print("bbb", std)
-            std = 0.02
+            std = self.config.init_std
             module.weight.data.normal_(mean=0.0, std=std)
             if module.bias is not None:
                 module.bias.data.zero_()
         elif isinstance(module, nn.Embedding):
-            embedding_dim = module.weight.shape[-1]
-            std = embedding_dim**-0.5
-            print(module)
-            print("aaa", std)
-            std = 0.02
+            std = self.config.init_std
             module.weight.data.normal_(mean=0.0, std=std)
             if module.padding_idx is not None:
                 module.weight.data[module.padding_idx].zero_()
