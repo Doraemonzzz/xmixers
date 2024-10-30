[Bug Fix] update attention mask in attention, fix ape bug, update gpt name

xmixers/modules/pes/sin_cos_pe.py
@@ -56,11 +56,11 @@ class SinCosPe(nn.Module):
         self.pe = pe
 
     def forward(self, x, shape=None, offset=0):
-        if self.pe.shape[0] == 0 or self.pe.shape[0] < x.shape[1]:
-            self.get_pe(x, shape)
+        if self.pe.shape[0] == 0 or self.pe.shape[0] < (offset + x.shape[-2]):
+            assert len(x.shape) == 3, "only support 1d case"
+            self.get_pe(x, [offset + x.shape[-2]])
         start = offset
         end = offset + x.shape[1]
-
         x = (
             x
             + self.pe[

xmixers/modules/token_mixers/vanilla_attention/attention.py
@@ -90,7 +90,9 @@ class Attention(nn.Module):
         if past_key_values is not None:
             k, v = past_key_values.update(k, v, self.layer_idx)
 
-        if attention_mask is None:
+        if (
+            attention_mask is None or attention_mask.all()
+        ):  # if attention mask is None or all elements are True, use sdpa
             # use causal when training or evaluation(not for generation)
             is_causal = True if self.training or q.shape[-2] == k.shape[-2] else False
             output = F.scaled_dot_product_attention(q, k, v, is_causal=is_causal)