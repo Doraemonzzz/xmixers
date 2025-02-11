fb98e1dc76961801dc85509c863685d21cd747b3
Doraemonzzz <715073608@qq.com>
2025-02-11 08:51:45 +0000

    [Feature add] add varlen support for attn

--- xmixers/modules/token_mixers/vanilla_attention/attention.py
+++ xmixers/modules/token_mixers/vanilla_attention/attention.py
@@ -11,9 +11,13 @@ from xmixers.utils import XMIXERS_DEBUG, _initialize_weights, print_params
 from ...pes import Lrpe
 
 try:
-    from flash_attn import flash_attn_func
+    from flash_attn import flash_attn_func, flash_attn_varlen_func
+    from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input
 except:
     flash_attn_func = None
+    index_first_axis = None
+    pad_input = None
+    unpad_input = None
 
 
 class Attention(nn.Module):
@@ -88,6 +92,7 @@ class Attention(nn.Module):
         **kwargs,
     ):
         # x: b n d
+        b, n, d = x.shape
         # linear map
         q = self.q_proj(x)
         k = self.k_proj(x)
@@ -103,6 +108,10 @@ class Attention(nn.Module):
         if past_key_values is not None:
             q_offset = past_key_values.get_seq_length(self.layer_idx)
 
+        if self.use_lrpe:
+            q = self.lrpe(q, offset=q_offset)
+            k = self.lrpe(k, offset=q_offset)
+
         # cache update
         if past_key_values is not None:
             k, v = past_key_values.update(
@@ -111,16 +120,12 @@ class Attention(nn.Module):
                 offset=x.shape[-2],
             )["attn_state"]
 
-        if self.use_lrpe:
-            q = self.lrpe(q, offset=q_offset)
-            k = self.lrpe(k)
-
+        causal = True if self.training or q.shape[-3] == k.shape[-3] else False
+        window_size = (self.window_size, 0) if self.window_size > 0 else (-1, -1)
         if (
             attention_mask is None or attention_mask.all()
         ):  # if attention mask is None or all elements are True, use sdpa
             # use causal when training or evaluation(not for generation) or prefill
-            causal = True if self.training or q.shape[-3] == k.shape[-3] else False
-            window_size = (self.window_size, 0) if self.window_size > 0 else (-1, -1)
             if k.shape[-1] == v.shape[-1]:
                 output = flash_attn_func(
                     q, k, v, causal=causal, window_size=window_size
@@ -133,7 +138,23 @@ class Attention(nn.Module):
                 output = F.scaled_dot_product_attention(q, k, v, is_causal=causal)
                 output = rearrange(output, "... h n d -> ... n h d")
         else:
-            assert False, "flash_attn_varlen_qkvpacked_func current not support"
+            q, k, v, indices_q, cu_seq_lens, max_seq_lens = self._upad_input(
+                q=q, k=k, v=v, attention_mask=attention_mask, q_len=n
+            )
+            cu_seqlens_q, cu_seqlens_k = cu_seq_lens
+            max_seqlen_q, max_seqlen_k = max_seq_lens
+            output = flash_attn_varlen_func(
+                q=q,
+                k=k,
+                v=v,
+                cu_seqlens_q=cu_seqlens_q,
+                cu_seqlens_k=cu_seqlens_k,
+                max_seqlen_q=max_seqlen_q,
+                max_seqlen_k=max_seqlen_k,
+                causal=causal,
+                window_size=window_size,
+            )
+            output = pad_input(output, indices_q, b, n)
 
         # reshape
         output = rearrange(output, "... n h d -> ... n (h d)")
@@ -141,3 +162,46 @@ class Attention(nn.Module):
         output = self.o_proj(output)
 
         return output, past_key_values
+
+    # credit to: https://github.com/fla-org/flash-linear-attention/blob/main/fla/layers/attn.py
+    def _upad_input(self, q, k, v, attention_mask, q_len):
+        seqlens = attention_mask.sum(-1, dtype=torch.int32)
+        indices_k = torch.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
+        max_seqlen_k = seqlens.max().item()
+        cu_seqlens_k = F.pad(torch.cumsum(seqlens, dim=0, dtype=torch.int32), (1, 0))
+        batch_size, seq_len, num_key_value_heads, head_dim = k.shape
+
+        k = index_first_axis(
+            k.reshape(batch_size * seq_len, num_key_value_heads, head_dim), indices_k
+        )
+        v = index_first_axis(
+            v.reshape(batch_size * seq_len, num_key_value_heads, head_dim), indices_k
+        )
+        if q_len == seq_len:
+            q = index_first_axis(
+                q.reshape(batch_size * seq_len, self.num_heads, head_dim), indices_k
+            )
+            cu_seqlens_q = cu_seqlens_k
+            max_seqlen_q = max_seqlen_k
+            indices_q = indices_k
+        elif q_len == 1:
+            max_seqlen_q = 1
+            # There is a memcpy here, that is very bad.
+            cu_seqlens_q = torch.arange(
+                batch_size + 1, dtype=torch.int32, device=q.device
+            )
+            indices_q = cu_seqlens_q[:-1]
+            q = q.squeeze(1)
+        else:
+            # The -q_len: slice assumes left padding.
+            attention_mask = attention_mask[:, -q_len:]
+            q, indices_q, cu_seqlens_q, max_seqlen_q = unpad_input(q, attention_mask)
+
+        return (
+            q,
+            k,
+            v,
+            indices_q,
+            (cu_seqlens_q, cu_seqlens_k),
+            (max_seqlen_q, max_seqlen_k),
+        )
